5af369fc54e09a73235a3f54bb12d2bbf2499958
Stepa Strunkov <stepa.strunkov@gmail.com>
2021-12-21 23:31:23 +0300

    Edited start message

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ def get_fact():
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await bot.send_message(message.from_user.id,
-                           "Привет, пришлю рандомный факт")
+                           "Привет, пришлю рандомный факт в ответ на "
+                           "любое твое сообщение.")
 
 
 @dp.message_handler()
